7902fb7a621fbd61f8a07d5ba46759e655ed3a15
arjun-reboostify <reboostify@gmail.com>
2025-02-09 02:42:26 +0530

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import {Button} from '@/components/ui/button'
-
+import Themetoggle from '@/components/shared/themetoggle'
 export default function Home() {
   return (
     <div>
+<Themetoggle/>
       <h1 className="font-barlow">welcome to reboostify</h1>
       <h1 className="">welcome to reboostify</h1>
       <Button variant="outline">b</Button>
--- src/components/shared/themetoggle.tsx
+++ src/components/shared/themetoggle.tsx
@@ -1,14 +1,22 @@
+"use client";
 import React from 'react'
-import { DropdownMenu } from '../ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from '../ui/dropdown-menu'
 import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
 import { Button } from '../ui/button'
+import { useTheme } from 'next-themes';
 
 export default function themetoggle() {
+    const { theme, setTheme } = useTheme();
   return (
     <DropdownMenu>
         <DropdownMenuTrigger asChild>
 <Button></Button>
         </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+            <DropdownMenuItem onClick={()=>setTheme("light")}>light</DropdownMenuItem>
+            <DropdownMenuItem onClick={()=>setTheme("dark")}>dark</DropdownMenuItem>
+            <DropdownMenuItem onClick={()=>setTheme("system")}>ystem</DropdownMenuItem>
+        </DropdownMenuContent>
     </DropdownMenu>
   )
 }
